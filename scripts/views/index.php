1fbc03d0889a3066b7391c16400c8e6095ce140a
Shoxsz <paulomarcio18p1@gmail.com>
2020-07-28 09:28:14 -0300

    adding some css styling

--- scripts/views/index.php
+++ scripts/views/index.php
@@ -1,38 +1,40 @@
 
   <body>
     <link rel="stylesheet" href="/ibijus/assets/styles/index.css"/>
-    <div>
     <div class="content">
-      <h2>Locais que já visitei</h2>
-      <table class="locais-table card-shadow">
-        <thead>
-          <tr>
-            <td>Nome</td>
-            <td>Data da visita</td>
-            <td>Opções</td>
-          </tr>
-        </thead>
-        <tbody>
-          <?php foreach($locais as $local): ?>
-          <tr class="local-row<?php echo $local->uf == "MG" ? " local-mg" : " " ?>">
-            <td><?php echo $local->nome; ?></td>
-            <td><?php echo $local->data; ?></td>
-            <td>
-              <div class="table-options">
-                <div class="table-option delete-option" local="<?php echo $local->id?>">
-                  <i class="fa fa-times"></i>
+      <div class="backimage"></div>
+      <div class="locais-table card-shadow">
+        <h1 class="table-header-text">Meus Locais</h1>
+        <table>
+          <thead>
+            <tr>
+              <td>Nome</td>
+              <td>Data da visita</td>
+              <td>Opções</td>
+            </tr>
+          </thead>
+          <tbody>
+            <?php foreach($locais as $local): ?>
+            <tr class="local-row<?php echo $local->uf == "MG" ? " local-mg" : " " ?>">
+              <td><?php echo $local->nome; ?></td>
+              <td><?php echo $local->data; ?></td>
+              <td>
+                <div class="table-options">
+                  <div class="table-option delete-option" local="<?php echo $local->id?>">
+                    <i class="fa fa-times"></i>
+                  </div>
+                  <div class="table-option edit-option" local="<?php echo $local->id?>">
+                    <i class="fa fa-edit"></i>
+                  </div>
                 </div>
-                <div class="table-option edit-option" local="<?php echo $local->id?>">
-                  <i class="fa fa-edit"></i>
-                </div>
-              </div>
-            </td>
-          </tr>
-          <?php endforeach;?>
-        </tbody>
-      </table>
+              </td>
+            </tr>
+            <?php endforeach;?>
+          </tbody>
+        </table>
+      </div>
       <a href="/ibijus/novo-local">
-        <button class="add-button"><i class="fa fa-add"></i>Adicionar</button>
+        <button><i class="fa fa-add"></i>Adicionar</button>
       </a>
     </div>
   </body>
--- scripts/views/local-form.php
+++ scripts/views/local-form.php
@@ -1,56 +1,62 @@
 <body>
   <link rel="stylesheet" href="/ibijus/assets/styles/local-form.css"/>
   <div class="content">
-    <h2><?php echo isset($local) ? "Editar local" : "Novo local" ?></h2>
+    <div class="backimage"></div>
     <form class="card-shadow" id="cadastro_form" method="post">
-      <?php if(isset($local)):?>
-      <input id="local-id" type="hidden" value="<?php echo $local->id?>" />
-      <?php endif;?>
+      <h1 class="backimage-text"><?php echo isset($local) ? "Editar local" : "Novo local" ?></h1>
+      <div class="form-content">
+        <?php if(isset($local)):?>
+        <input id="local-id" type="hidden" value="<?php echo $local->id?>" />
+        <?php endif;?>
 
-      <div class="input-element">
-        Nome:<br/>
-        <input type="text" value="<?php echo isset($local) ? $local->nome : ""?>" id="nome" name="nome" placeholder="Nome"/><br/>
-      </div>
-      
-      <div class="input-element">
-        CEP:<br/>
-        <input type="text" value="<?php echo isset($local) ? $local->cep : ""?>" id="cep" name="cep" placeholder="CEP"/><br/>
-      </div>
-      <div class="input-element">
-        Logradouro:<br/>
-        <input type="text" value="<?php echo isset($local) ? $local->logradouro : ""?>" id="logradouro" name="logradouro" placeholder="Logradouro"/><br/>
-      </div>
+        <div class="input-element">
+          Nome:<br/>
+          <input type="text" value="<?php echo isset($local) ? $local->nome : ""?>" id="nome" name="nome" placeholder="Nome"/><br/>
+        </div>
+        
+        <div class="input-element">
+          CEP:<br/>
+          <input type="text" value="<?php echo isset($local) ? $local->cep : ""?>" id="cep" name="cep" placeholder="CEP"/><br/>
+        </div>
+        <div class="input-element">
+          Logradouro:<br/>
+          <input type="text" value="<?php echo isset($local) ? $local->logradouro : ""?>" id="logradouro" name="logradouro" placeholder="Logradouro"/><br/>
+        </div>
 
-      <div class="input-element">
-        Complemento:<br/>
-        <input type="text" value="<?php echo isset($local) ? $local->complemento : ""?>" id="complemento" name="complemento" placeholder="Complemento"/><br/>
-      </div>
+        <div class="input-element">
+          Complemento:<br/>
+          <input type="text" value="<?php echo isset($local) ? $local->complemento : ""?>" id="complemento" name="complemento" placeholder="Complemento"/><br/>
+        </div>
 
-      <div class="input-element">
-        Numero:<br/>
-        <input type="text" value="<?php echo isset($local) ? $local->numero : ""?>" id="numero" name="numero" placeholder="Número"/><br/>
-      </div>
-      
-      <div class="input-element">
-        Bairro:<br/>
-        <input type="text" value="<?php echo isset($local) ? $local->bairro : ""?>" id="bairro" name="bairro" placeholder="Bairro"/><br/>
-      </div>
+        <div class="input-element">
+          Numero:<br/>
+          <input type="text" value="<?php echo isset($local) ? $local->numero : ""?>" id="numero" name="numero" placeholder="Número"/><br/>
+        </div>
+        
+        <div class="input-element">
+          Bairro:<br/>
+          <input type="text" value="<?php echo isset($local) ? $local->bairro : ""?>" id="bairro" name="bairro" placeholder="Bairro"/><br/>
+        </div>
 
-      <div class="input-element">
-        UF:<br/>
-        <input type="text" value="<?php echo isset($local) ? $local->uf : ""?>" id="uf" name="uf" placeholder="UF"/><br/>
-      </div>
+        <div class="input-element">
+          UF:<br/>
+          <input type="text" value="<?php echo isset($local) ? $local->uf : ""?>" id="uf" name="uf" placeholder="UF"/><br/>
+        </div>
 
-      <div class="input-element">
-        Cidade:<br/>
-        <input type="text" value="<?php echo isset($local) ? $local->cidade : ""?>" id="cidade" name="cidade" placeholder="Cidade"/><br/>
-      </div>
+        <div class="input-element">
+          Cidade:<br/>
+          <input type="text" value="<?php echo isset($local) ? $local->cidade : ""?>" id="cidade" name="cidade" placeholder="Cidade"/><br/>
+        </div>
 
-      <div class="input-element">
-        Data:<br/>
-        <input type="text" value="<?php echo isset($local) ? $local->data : ""?>" id="data" name="data" placeholder="Data"/><br/>
+        <div class="input-element">
+          Data:<br/>
+          <input type="text" value="<?php echo isset($local) ? $local->data : ""?>" id="data" name="data" placeholder="Data"/><br/>
+        </div>
+        <div class="input-element">
+          <input class="form-input-button" type="submit" value="Confirmar"/>
+          <a href="/ibijus/"><button type="button">Cancelar</button></a>
+        </div>
       </div>
-      <input class="form-input-button" type="submit" value="Confirmar"/>
     </form>
   </div>
 </body>
